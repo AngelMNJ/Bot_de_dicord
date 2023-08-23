cb5fd2cce3ac9a2c6c41fdba4d90b1854e266fee
AngelMNJ <130695153+AngelMNJ@users.noreply.github.com>
2023-08-23 18:21:41 +0200

    Update discord_3.py

--- discord_3.py
+++ discord_3.py
@@ -2,11 +2,13 @@ import discord
 from discord.ext import commands
 import random
 import asyncio
+import math
+import os
 
 intents = discord.Intents.default()
 intents.message_content = True
 
-bot = commands.Bot(command_prefix='$', intents=intents)
+bot = commands.Bot(command_prefix='!', intents=intents)
 
 @bot.event
 async def on_ready():
@@ -18,7 +20,7 @@ async def hello(ctx):
 
 @bot.command()
 async def guess(ctx):
-    await ctx.send('Dime un numero del 1 al 10.')
+    await ctx.send('Guess a number between 1 and 10.')
 
     def is_correct(m):
         return m.author == ctx.author and m.content.isdigit()
@@ -35,4 +37,30 @@ async def guess(ctx):
     else:
         await ctx.send(f'Oops. It is actually {answer}.')
 
-bot.run("MTEwMTIwNTk4MDE1NjUzMDcxOQ.GOM5Dn.fJSWodRWaVHnl_GPvKThOv172kRABluY5j4Qss")
+@bot.command()
+async def hola(ctx):
+    await ctx.send("Good morning, or afternoon or night, I don't know what hour it is.")
+
+@bot.command()
+async def mem(ctx):
+    imagen = random.choice(os.listdir('images'))
+    with open(f'images/{imagen}', 'rb') as f:
+        # ¡Vamos a almacenar el archivo de la biblioteca Discord convertido en esta variable!
+        picture = discord.File(f)
+    # A continuación, podemos enviar este archivo como parámetro.
+    await ctx.send(file=picture)
+
+@bot.command()   
+async def solve(ctx, a: float, b: float, c: float):
+    try:
+        raiz = b**2 - 4*a*c
+        if raiz >= 0:
+            x_1 = (-b + math.sqrt(b**2 - 4*a*c)) / (2*a)
+            x_2 = (-b - math.sqrt(b**2 - 4*a*c)) / (2*a)
+            await ctx.send(f"The solutions are: {x_1} and {x_2}")
+        else:
+            await ctx.send("The equation has no real solutions.")
+    except ValueError:
+        await ctx.send("Invalid input. Please provide valid numerical values.")
+
+bot.run("MTEwMTIwNTk4MDE1NjUzMDcxOQ.GBLWim.XLnJO93W6orXC_IhRMwFvC-I8b44mrUCByYbpA")
